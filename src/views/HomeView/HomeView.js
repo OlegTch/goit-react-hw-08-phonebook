a62f5d3cceaebbd24d6f4dbeb301ce9aad6157fc
OlegTch <Olgard984@gmail.com>
2022-01-30 23:37:09 +0200

    add img

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -2,7 +2,7 @@ import styles from './HomeView.module.css';
 
 const HomeView = () => {
   return (
-    <div>
+    <div className={styles.thumb}>
       <h1 className={styles.title}>This is PHONEBOOK</h1>
       <p className={styles.text}>Please, register or login!</p>
     </div>
